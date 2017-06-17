e0d8b268afb1051176589444e4ec6f0eefa685a1
jcamhi <jeremshy@h3lp.fr>
2017-06-17 11:40:11 +0200

    Adding Render Machine

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@ int main() {
 	init_ncurse();
 	while (1) {
 		mvaddch(4, 4, 'c');
-	char ch = getch();
-
-	if (ch == 27) {
-		endwin();
-		return (0);
+		char ch = getch();
+		if (ch == 27) {
+			endwin();
+			return (0);
+		}
 	}
-}
 }
