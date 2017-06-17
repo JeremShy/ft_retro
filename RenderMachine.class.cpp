100c0880d4b79cfc61be16270f6e82e6733111af
jcamhi <jeremshy@h3lp.fr>
2017-06-17 13:51:04 +0200

    Completing the getAmount class

--- RenderMachine.class.cpp
+++ RenderMachine.class.cpp
@@ -1,4 +1,5 @@
 #include "RenderMachine.class.hpp"
+#include <iostream>
 
 RenderMachine::RenderMachine(void) : _tab(NULL), _amount(0){
 }
@@ -32,4 +33,72 @@ void	RenderMachine::renderAll(void) {
 	for (unsigned int i = 0; i < this->_amount; i++) {
 		this->_tab[i]->display();
 	}
+}
+
+void	RenderMachine::addPrintable(unsigned int idx, IPrintable *obj) { // TO DO : Tester quand les iprintable seront fonctionels.
+	if (idx > this->_amount + 1) {
+		std::cout << "There was an error while trying to ADD a printable on the RenderMachine at position " << idx << ". There is only " << this->_amount << " printables in the table." << std::endl;
+		return ;
+	}
+	if (this->_amount == 0) {
+		if (this->_tab != NULL) { //_tab is supposed to be null if _amount == 0, but still..
+			delete [] this->_tab;
+		}
+		this->_tab = new IPrintable*[1];
+		this->_tab[0] = obj;
+		this->_amount = 1;
+		return ;
+	}
+	IPrintable **new_tab = new IPrintable*[this->_amount + 1];
+	unsigned int i;
+
+	for (i = 0; i < idx; i++) {
+		new_tab[i] = this->_tab[i];
+	}
+	new_tab[i] = obj;
+	i++;
+	for (i = i; i < this->_amount; i++) {
+		new_tab[i] =this->_tab[i - 1];
+	}
+	delete [] this->_tab;
+	this->_tab = new_tab;
+}
+
+void	RenderMachine::addPrintable(IPrintable *obj) {
+	this->addPrintable(this->_amount + 1, obj);
+}
+
+void	RenderMachine::removePrintable(unsigned int idx) {
+	if (idx >= this->_amount) {
+		std::cout << "There was an error while trying to REMOVE a printable on the RenderMachine at position " << idx << ". There is only " << this->_amount << " printables in the table." << std::endl;
+		return ;
+	}
+
+	IPrintable		**new_tab = new IPrintable*[this->_amount - 1];
+	unsigned int	i;
+
+	for (int i = 0; i < idx; i ++) {
+		new_tab[i] =this->_tab[i - 1];
+	}
+	i++;
+	for (i = i; i < this->_amount; i++) {
+		new_tab[i - 1] = this->_tab[i];
+	}
+	if (this->_tab)
+		delete [] this->_tab;
+	else
+		std::cout << "You aren't supposed to be here. There probably was an error.. " << std::endl;
+	this->_tab = new_tab;
+}
+
+IPrintable	*RenderMachine::getPrintable(unsigned int idx) {
+	if (idx >= this->_amount) {
+		std::cout << "There was an error while trying to GET a printable on the RenderMachine at position " << idx << ". There is only " << this->_amount << " printables in the table. Returning NULL." << std::endl;
+		return NULL;
+	}
+	return (this->_tab[idx]);
+}
+
+unsigned int	RenderMachine::getAmount(void) const {
+	return (this->_amount);
 }
--- RenderMachine.class.hpp
+++ RenderMachine.class.hpp
@@ -14,9 +14,12 @@ public:
 	RenderMachine & operator=(RenderMachine const & rhs);
 
 	void	renderAll(void);
-	void	addPrintable(unsigned int id);
+	void	addPrintable(unsigned int id, IPrintable *obj);
+	void	addPrintable(IPrintable *obj);
 	void	removePrintable(unsigned int id);
+	IPrintable	*getPrintable(unsigned int _id);
 
+	unsigned int	getAmount(void) const;
 private:
 	IPrintable		**_tab;
 	unsigned int	_amount;
