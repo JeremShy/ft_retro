9e8d05a69f1dfd39019b4ba9c2548d70ff6a603a
Fabien SACREPEIGNE <fsacrepe@e2r3p10.42.fr>
2017-06-17 11:02:47 +0200

    GameEntity.class.hpp backbone

---
+++ GameEntity.class.hpp
@@ -0,0 +1,36 @@
+#ifndef GAME_ENTITY_HPP
+# define GAME_ENTITY_HPP
+
+class GameEntity
+{
+public:
+	GameEntity(char const model);
+	GameEntity(GameEntity const & cpy);
+
+	GameEntity & 	operator=(GameEntity const &);
+
+	void			takeDamage(unsigned int);
+
+	char const		getModel(void) const;
+	unsigned int 	getHealth(void) const;
+	unsigned int 	getDamage(void) const;
+	unsigned int 	getPositionX(void) const;
+	unsigned int 	getPositionY(void) const;
+
+	void			setHealth(unsigned int);
+	void			setDamage(unsigned int);
+	void			setPositionX(unsigned int);
+	void			setPositionY(unsigned int);
+
+	virtual ~GameEntity();
+
+private:
+	char const		_model;
+	unsigned int 	_x;
+	unsigned int 	_y;
+	unsigned int 	_health;
+	unsigned int 	_damage;
+	
+};
+
+#endif
